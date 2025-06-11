db5e6a19e56e72df2998c067be8f9378b10f5641
scnew8013 <scnew8013@gmail.com>
2025-06-11 01:16:13 +0000

    frebase

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-25.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
@@ -13,6 +13,7 @@
     # pkgs.nodePackages.nodemon
     pkgs.corepack_latest
     pkgs.fish
+    pkgs.nodejs_22
   ];
 
   # Sets environment variables in the workspace
